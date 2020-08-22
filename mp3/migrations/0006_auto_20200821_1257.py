97f0855ae9e67500414a8f0b458ebf98c48f378a
BuildTools <unconfigured@null.spigotmc.org>
2020-08-22 11:15:53 +0900

    removed id

--- mp3/migrations/0006_auto_20200821_1257.py
+++ mp3/migrations/0006_auto_20200821_1257.py
@@ -20,9 +20,5 @@ class Migration(migrations.Migration):
             model_name='audiofile',
             name='song',
         ),
-        migrations.AlterField(
-            model_name='audiofile',
-            name='id',
-            field=models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False),
-        ),
+        
     ]
