e29c214af0e464c067e2466da4cdf11d0a21b058
BuildTools <unconfigured@null.spigotmc.org>
2020-08-25 00:18:19 +0900

    check if audiofile exist

--- mp3/metadata.py
+++ mp3/metadata.py
@@ -26,48 +26,49 @@ def get_metadata(songName, path):
     if path:
         audiofile = eyed3.load(path)
         # pprint(vars(audiofile.tag))
+        if audiofile:
 
-        if audiofile.info and audiofile.info.time_secs:
-            metadata["duration"]=duration_from_seconds(audiofile.info.time_secs)[-5:]
-            print("duration: %s" % duration_from_seconds(audiofile.info.time_secs)[-5:])
+            if audiofile.info and audiofile.info.time_secs:
+                metadata["duration"]=duration_from_seconds(audiofile.info.time_secs)[-5:]
+                print("duration: %s" % duration_from_seconds(audiofile.info.time_secs)[-5:])
 
-        if audiofile.tag:
-            # 구글 스토리지에 앨범 커버를 저장함
-            if audiofile.tag.images:
-                for image in audiofile.tag.images:
-                    coverName=audiofile.tag.title or songName
-                    path_on_cloud = gsCoverFolder+"/"+coverName +".jpg"
-                    storageInfos["storage"].child(path_on_cloud).put(image.image_data)
+            if audiofile.tag:
+                # 구글 스토리지에 앨범 커버를 저장함
+                if audiofile.tag.images:
+                    for image in audiofile.tag.images:
+                        coverName=audiofile.tag.title or songName
+                        path_on_cloud = gsCoverFolder+"/"+coverName +".jpg"
+                        storageInfos["storage"].child(path_on_cloud).put(image.image_data)
 
-                    metadata["cover"] = storageInfos["storage"].child(
-                        path_on_cloud).get_url(user['idToken'])
-        
-            # 메타데이터 추출
-            metadata["album"]=audiofile.tag.album or ""
-            metadata["title"]=audiofile.tag.title or songName
-            metadata["artist"]=audiofile.tag.artist or ""
-            metadata["release"]=audiofile.tag.getBestDate()  or ""
+                        metadata["cover"] = storageInfos["storage"].child(
+                            path_on_cloud).get_url(user['idToken'])
+            
+                # 메타데이터 추출
+                metadata["album"]=audiofile.tag.album or ""
+                metadata["title"]=audiofile.tag.title or songName
+                metadata["artist"]=audiofile.tag.artist or ""
+                metadata["release"]=audiofile.tag.getBestDate()  or ""
 
-            if audiofile.tag.genre:
-                metadata["genre"]=audiofile.tag.genre # 인덱스 오류 자주남 (에러처리)
+                if audiofile.tag.genre:
+                    metadata["genre"]=audiofile.tag.genre # 인덱스 오류 자주남 (에러처리)
 
-            if audiofile.tag.lyrics and audiofile.tag.lyrics[0] and audiofile.tag.lyrics[0].text:
-                metadata["lyrics"]=audiofile.tag.lyrics[0].text 
-                print("lyrics: %s" % audiofile.tag.lyrics[0].text)
+                if audiofile.tag.lyrics and audiofile.tag.lyrics[0] and audiofile.tag.lyrics[0].text:
+                    metadata["lyrics"]=audiofile.tag.lyrics[0].text 
+                    print("lyrics: %s" % audiofile.tag.lyrics[0].text)
 
-            # 콘솔에 추출한 태그 정보 출력
-            print("------------------------------ 태그 정보 ------------------------------")
-            print("album: %s" % audiofile.tag.album)
-            print("title: %s" % audiofile.tag.title)
-            print("artist: %s" % audiofile.tag.artist)
-            print("genre: %s" % audiofile.tag.genre)
-            print("release: %s" % audiofile.tag.getBestDate())
-            print("------------------------------------------------------------------------")
+                # 콘솔에 추출한 태그 정보 출력
+                print("------------------------------ 태그 정보 ------------------------------")
+                print("album: %s" % audiofile.tag.album)
+                print("title: %s" % audiofile.tag.title)
+                print("artist: %s" % audiofile.tag.artist)
+                print("genre: %s" % audiofile.tag.genre)
+                print("release: %s" % audiofile.tag.getBestDate())
+                print("------------------------------------------------------------------------")
 
-            # for image in audiofile.tag.images:
-            #     image_file = open(settings.STATICFILES_DIRS[0]+'/covers/'+songName, "wb")
-            #     # print("Writing image file: {0} - {1}({2}).jpg".format(artist_name, album_name, image.picture_type))
-            #     image_file.write(image.image_data)
-            #     image_file.close()
+                # for image in audiofile.tag.images:
+                #     image_file = open(settings.STATICFILES_DIRS[0]+'/covers/'+songName, "wb")
+                #     # print("Writing image file: {0} - {1}({2}).jpg".format(artist_name, album_name, image.picture_type))
+                #     image_file.write(image.image_data)
+                #     image_file.close()
 
     return metadata
